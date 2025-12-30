e804a7925ec44fc6d6253838e8789293df106483
Mohamed Nagoor <mdnagoor2004@gmail.com>
2025-12-30 11:53:45 +0530

    feat: Add main UI5 component for requisitions application.

---
+++ app/requisitions/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("coe.demo.requisitions.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
